c1477111170f7172a3e609140d49493e18d95958
Francesc Obrador <obrador.francesc@gmail.com>
2025-02-09 18:27:39 +0100

    Patron 1 - Abstract factory

---
+++ Practica 4/1_AbstractFactory/FabricaElectrico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatronesGOF.AbstractFactory
+{
+    public class FabricaElectrico : IFabricaVehiculo
+    {
+
+        public IVehiculo CrearAutomovil()
+        {
+            return new Automovil(new Electricidad());
+        }
+
+        public IVehiculo CrearScooter()
+        {
+            return new Scooter(new Electricidad());
+        }
+    }
+}
---
+++ Practica 4/1_AbstractFactory/IVehiculo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatronesGOF.AbstractFactory
+{
+    public interface IVehiculo
+    {
+        void Mostrar();
+    }
+}
